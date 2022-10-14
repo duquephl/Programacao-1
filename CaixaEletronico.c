291f742e0e42b6156006fe4b2b0bfd0f81788535
Pedro H. Duque <euphlduque@gmail.com>
2022-10-14 19:09:36 -0300

    TerminoTb1

--- CaixaEletronico.c
+++ CaixaEletronico.c
@@ -23,22 +23,42 @@ int main(void)
 
         quantNotas200 = valor / 200;
         valor = valor % 200;
+        if (quantNotas200 > 0 && (valor == 1 || valor==3))
+        {
+            quantNotas200--;
+            valor += 200;
+        }
         quantNotas100 = valor / 100;
         valor = valor % 100;
+        if (quantNotas100 > 0 && (valor == 1 || valor==3))
+        {
+            quantNotas100--;
+            valor += 100;
+        }
         quantNotas50 = valor / 50;
         valor = valor % 50;
+        if (quantNotas50 > 0 && (valor == 1 || valor==3))
+        {
+            quantNotas50--;
+            valor += 50;
+        }
         quantNotas20 = valor / 20;
         valor = valor % 20;
+        if (quantNotas20 > 0 && (valor == 1 || valor==3))
+        {
+            quantNotas20--;
+            valor += 20;
+        }
         quantNotas10 = valor / 10;
         valor = valor % 10;
-        if (quantNotas10 > 0 && valor != 0)
+        if (quantNotas10 > 0 && (valor == 1 || valor==3))
         {
             quantNotas10--;
             valor += 10;
         }
         quantNotas5 = valor / 5;
         valor = valor % 5;
-        if (quantNotas5 > 0 && valor != 0)
+        if (quantNotas5 > 0 && (valor == 1 || valor==3))
         {
             quantNotas5--;
             valor += 5;
@@ -49,12 +69,10 @@ int main(void)
         if (valor != 0)
         {
             printf("Não é possível sacar este valor.\n");
+            return -1;
         }
-        else
-        {
             printf("\n\nVocê receberá:\n%d notas de 200 reais.\n%d notas de 100 reais.\n%d notas de 50 reais.\n%d notas de 20 reais.\n", quantNotas200, quantNotas100, quantNotas50, quantNotas20);
             printf("%d notas de 10 reais.\n%d notas de 5 reais.\n%d notas de 2 reais.\n", quantNotas10, quantNotas5, quantNotas2);
-        }
     }
 
     return 0;
---
+++ cpf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void){
+    int k,r,l,digito, d10,d11,mult=0;
+    return 0;
+}
