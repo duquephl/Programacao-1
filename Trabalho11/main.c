fbf75a46303365c25d4d0ff9606eeb8fcb9b8009
Pedro Duque <euphlduque@gmail.com>
2023-02-08 22:00:47 -0300

    Trabalho 11 correção

--- Trabalho11/main.c
+++ Trabalho11/main.c
@@ -1,7 +1,15 @@
+/*Trabalho de Programação I 
+UFES - SI 2022/2
+Professor: Paulo Nunes
+Alunos:Pedro Duque, Cássio Alcure, Estéfani Eller
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  FILE *arquivo = fopen("seqinteiros.dat", "r+");
+  FILE *arquivo = fopen("seqinteiros.dat", "r");
+  FILE *arquivo2 = fopen("piramidif.dat", "w");
   int numero, quantNum=0;
   int **matriz;
 
@@ -15,24 +23,43 @@ int main(void) {
     quantNum++;
   }
   printf("\n");
+  fclose(arquivo);
+
+  /// alocação da matriz
   matriz = (int**) malloc(quantNum * sizeof(int*));
-  for (int i = 0; i < quantNum-i; i++) {
-    matriz[i] = (int*) malloc(quantNum * sizeof(int));
+  for (int i = 0; i < quantNum; i++) {
+    matriz[i] = (int*) malloc((quantNum-i) * sizeof(int));
+  }
+  arquivo = fopen("seqinteiros.dat", "r");
+  /// alocação dos numeros na matriz
+  quantNum = 0;
+  while (fscanf(arquivo, "%d", &numero) != EOF){
+    matriz[0][quantNum] = numero;
+    quantNum++;
   }
 
-  for (int i = 0; i < quantNum-i; i++) {
-    for (int j = 0; j < quantNum-i; j++) {
-      matriz[i][j] = 0;
+  for(int i = 1; i < quantNum; i++){
+    for(int j = 0; j < quantNum-i; j++){
+      matriz[i][j] = matriz[i-1][j] - matriz[i-1][j+1];
     }
   }
+  ///estrever no arquivo
+  for (int i = 1; i < quantNum; i++) {
+    for (int j = 0; j < quantNum-i; j++) {
+      fprintf(arquivo2, "%d ", matriz[i][j]);
+    }
+    fprintf(arquivo2, "\n");
+  }
   ///imprimir matriz
-  for (int i = 0; i < quantNum-i; i++) {
+  for (int i = 0; i < quantNum; i++) {
     for (int j = 0; j < quantNum-i; j++) {
       printf("%d ", matriz[i][j]);
     }
     printf("\n");
   }
 
+  fclose(arquivo);
+  fclose(arquivo2);
 
   return 0;
 }
